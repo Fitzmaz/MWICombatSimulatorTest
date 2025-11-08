c8e9494947cb79d1aabb3a1c50cfc5330f9fc11b
foo <zcrzcr@qq.com>
2025-11-08 11:29:27 +0800

    fix revive

--- src/combatsimulator/combatUtilities.js
+++ src/combatsimulator/combatUtilities.js
@@ -321,7 +321,7 @@ class CombatUtilities {
         let amountHealed = target.addHitpoints(heal);
         target.combatDetails.currentManapoints = target.combatDetails.maxManapoints;
         target.clearCCs();
-        target.clearBuffs();
+        // target.clearBuffs();
 
         return amountHealed;
     }
